bff26e14610febedae1a9590d08a82a65a3842c9
František Navrkal <frantisek@revolt.bi>
2022-08-29 12:15:39 +0000

    Added Last Sync time usage bool parameter for bulk downloads

--- src/bingads_wrapper/bulk.py
+++ src/bingads_wrapper/bulk.py
@@ -5,6 +5,7 @@ from bingads.v13.bulk import DownloadParameters
 
 KEY_DATA_SCOPE = "data_scope"
 KEY_DOWNLOAD_ENTITIES = "download_entities"
+KEY_SINCE_LAST_SYNC_TIME = "since_last_sync_time"
 
 DOWNLOAD_REQUEST_TIMEOUT_PERIOD_MILLISECONDS = 60 * 1000
 REPORT_FILE_FORMAT = "Csv"
@@ -18,12 +19,14 @@ def create_download_parameters(
 ) -> DownloadParameters:
     data_scope: List[str] = config_dict[KEY_DATA_SCOPE]
     download_entities: List[str] = config_dict[KEY_DOWNLOAD_ENTITIES]
+    since_last_sync_time: bool = config_dict.get(KEY_SINCE_LAST_SYNC_TIME, False)
+    _last_sync_time_in_utc = last_sync_time_in_utc if since_last_sync_time else None
     return DownloadParameters(
         campaign_ids=None,
         data_scope=data_scope,
         download_entities=download_entities,
         file_type=REPORT_FILE_FORMAT,
-        last_sync_time_in_utc=last_sync_time_in_utc,
+        last_sync_time_in_utc=_last_sync_time_in_utc,
         result_file_directory=result_file_directory,
         result_file_name=result_file_name,
         overwrite_result_file=True,
